209ef2fa8557e129391ec4999b9a55a9ddf92a22
kuyur <kuyur@kuyur.info>
2018-11-07 19:10:26 +0900

    add question 02.

---
+++ 02/readme.md
@@ -0,0 +1,79 @@
+# Question
+
+Here are the rules of Lights Out Game:
+
+There is a matrix panel, holding a light in every cell.
+* There are two statuses, `on` and `off`, for each light. Let's mark `on` as `1`, and `off` as `0`.
+* We can touch the cell to toggle the status of light. However, the status of adjacent lights also will be toggled at the same time.
+* You can only touch one cell a time. One touching is calculated as 1 step. 
+
+For example, consider that we have a matrix panel now:
+
+    0 0 0 0 0
+    0 1 1 0 0
+    0 0 0 0 0
+
+If we touch the cell`[1, 2]` (row-index=1, column-index=2, indexes are 0-based.), the matrix will become:
+
+    0 0 1 0 0
+    0 0 0 1 0
+    0 0 1 0 0
+
+Because (the touched cell is marked as ❶):
+
+    0 0 0 0 0     0 0 0 0 0     0 0 1 0 0
+    0 1 1 0 0  →  0 1 ❶ 0 0  →  0 0 0 1 0
+    0 0 0 0 0     0 0 0 0 0     0 0 1 0 0
+
+Given a matrix panel with some lights on, find out the solution with minimum steps to turn off all lights. If there is no solution, return null.
+
+For example, given a matrix panel:
+
+    0 0 0 0 1
+    0 0 0 0 0
+    0 0 1 0 0
+    0 0 0 0 0
+
+One possible solution (with 4 steps) is:
+
+    [1, 4] → [2, 3] → [2, 4] → [3, 4]
+
+
+You can refer to https://en.wikipedia.org/wiki/Lights_Out_(game) for more detail.
+
+# Hint
+
+The order of steps is not important actually. Even we disrupt the order, it still be a valid solution, e.g.:
+
+    [2, 3] → [2, 4] → [1, 4] → [3, 4]
+
+The solution can be represented in another matrix:
+
+    0 0 0 0 0
+    0 0 0 0 1
+    0 0 0 1 1
+    0 0 0 0 1
+
+# Answer
+
+## English
+It is a Linear algebra mathematical problem actually.
+
+## Chinese
+这是一道线性代数数学题。
+
+# Code
+    var matrix = [
+      [0, 0, 0, 0, 1],
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0]
+    ];
+    var result = findSolution(4, 5, matrix);
+    console.log(result);
+    // [
+    //   [0, 0, 0, 0, 0],
+    //   [0, 0, 0, 0, 1],
+    //   [0, 0, 0, 1, 1],
+    //   [0, 0, 0, 0, 1]
+    // ]
---
+++ 02/solution.js
@@ -0,0 +1,179 @@
+// lights out game, see https://en.wikipedia.org/wiki/Lights_Out_(game)
+// It is a Linear algebra mathematical problem.
+// rank(A) < rank(A|B), no solution
+// rank(A) = rank(A|B) = n, 1 solution
+// rank(A) = rank(A|B) < n, countless solutions
+// see https://en.wikipedia.org/wiki/Augmented_matrix
+// Gaussian elimination, see https://en.wikipedia.org/wiki/Gaussian_elimination
+
+// execute bool XOR operation on each element in matrix to reduce coefficients.
+
+/**
+ * @param {number} m row count
+ * @param {number} n column count
+ * @param {Array.<Array.<number>>} matrix
+ * @return {Array.<Array.<number>>}
+ */
+function findSolution(m, n, matrix) {
+  var dimension = m * n;
+  var extended_matrix = []; // dimension x (dimension + 1)
+  var i, j;
+  for (i = 0; i < dimension; i++) {
+    var arr = [];
+    for (j = 0; j < dimension + 1; j++) {
+      arr[j] = 0;
+    }
+    extended_matrix[i] = arr;
+  }
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < n; j++) {
+      var index = i * n + j;
+      extended_matrix[index][dimension] = matrix[i][j];
+      var cells = findSiblings(i, j, m - 1, n - 1);
+      cells.forEach(function(cell) {
+        extended_matrix[index][cell.row * n + cell.column] = 1;
+      });
+    }
+  }
+  gaussian_elimination_part1(extended_matrix, dimension);
+  var ranks = rank(extended_matrix, dimension);
+  if (ranks.rank !== dimension || ranks.rank !== ranks.augmented) {
+    return null;
+  }
+  gaussian_elimination_part2(extended_matrix, dimension);
+  var solution = [];
+  for (i = 0; i < m; i++) {
+    solution[i] = [];
+    for (j = 0; j < n; j++) {
+      solution[i][j] = extended_matrix[i * n + j][dimension];
+    }
+  }
+  return solution;
+}
+
+/**
+ * @param {number} i 0-based row index
+ * @param {number} j 0-based column index
+ * @param {number} m 0-based max row index
+ * @param {number} n 0-based max column index
+ * @return {Array.<{row: number, column: number}>}
+ */
+function findSiblings(i, j, m, n) {
+  var result = [{
+    row: i,
+    column: j
+  }];
+  var top = i - 1;
+  if (top >= 0) {
+    result.push({
+      row: top,
+      column: j
+    });
+  }
+  var bottom = i + 1;
+  if (bottom <= m) {
+    result.push({
+      row: bottom,
+      column: j
+    });
+  }
+  var left = j - 1;
+  if (left >= 0) {
+    result.push({
+      row: i,
+      column: left
+    })
+  }
+  var right = j + 1;
+  if (right <= n) {
+    result.push({
+      row: i,
+      column: right
+    })
+  }
+  return result;
+};
+
+/**
+ * Gaussian elimination, part 1.
+ * @param {Array} matrix extended matrix(augmented matrix).
+ * @return {Array}
+ */
+function gaussian_elimination_part1(matrix, dimension) {
+  var kth = 0;
+  for (var i = 0; i < dimension; i++) {
+    while (!matrix[i][kth] && kth < dimension) {
+      for (var j = i + 1; j < dimension; j++) {
+        if (matrix[j][kth]) {
+          var temp = matrix[i];
+          matrix[i] = matrix[j];
+          matrix[j] = temp;
+          break;
+        }
+      }
+      if (matrix[i][kth]) {
+        break;
+      }
+      kth++;
+    }
+    if (kth >= dimension) {
+      break;
+    }
+    for (var k = i + 1; k < dimension; k++) {
+      if (matrix[k][kth]) {
+        for (var l = 0; l < dimension + 1; l++) {
+          matrix[k][l] = matrix[k][l] ^ matrix[i][l];
+        }
+      }
+    }
+    kth++;
+  }
+};
+
+/**
+ * Gaussian elimination, part 2.
+ * @param {Array} matrix extended matrix(augmented matrix).
+ * @return {Array}
+ */
+function gaussian_elimination_part2(matrix, dimension) {
+  var kth = dimension - 1;
+  for (var i = dimension - 2; i >= 0; i--, kth--) {
+    for (var j = dimension - 1; j >= kth; j--) {
+      if (matrix[i][j]) {
+        matrix[i][j] = matrix[i][j] ^ matrix[j][j];
+        matrix[i][dimension] = matrix[i][dimension] ^ matrix[j][dimension];
+      }
+    }
+  }
+};
+
+/**
+ * Evaluate the rank of coefficient matrix(系数矩阵) and augmented matrix(增广矩阵).
+ * @param {Array} matrix extended matrix (augmented matrix).
+ * @param {number} dimension
+ * @return {{
+ *   'rank': number,
+ *   'augmented': number
+ * }}}
+ */
+function rank(matrix, dimension) {
+  var extended = -1;
+  for (var i = dimension - 1; i >= 0; i--) {
+    var row = matrix[i];
+    for (var j = dimension - 1; j >= 0; j--) {
+      if (row[j]) {
+        return {
+          rank: i + 1,
+          augmented: extended === -1 ? i + 1 : extended
+        }
+      }
+      if (extended === -1 && row[dimension]) {
+        extended = i + 1;
+      }
+    }
+  }
+  return {
+    rank: 0,
+    augmented: extended === -1 ? 0 : extended
+  };
+};

a2e910c2300ff23196b43c0972b6a1c5ee304150
kuyur <kuyur@kuyur.info>
2021-06-14 01:28:06 +0900

    add comments.

--- leetcode/leetcode-023/solution-input-array.js
+++ leetcode/leetcode-023/solution-input-array.js
@@ -1,4 +1,17 @@
 /**
+ * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ *
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ * Example:
+ * Input: lists = [[1,4,5],[1,3,4],[2,6]]
+ * Output: [1,1,2,3,4,4,5,6]
+ */
+
+/**
+ * @author kuyur@kuyur.info
+ */
+
+/**
  * min-heap class.
  * @author kuyur@kuyur.info
  * @param {number} capacity
--- leetcode/leetcode-023/solution-input-linked-list.js
+++ leetcode/leetcode-023/solution-input-linked-list.js
@@ -1,4 +1,25 @@
 /**
+ * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ *
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ * Example:
+ * Input: lists = [[1,4,5],[1,3,4],[2,6]]
+ * Output: [1,1,2,3,4,4,5,6]
+ */
+
+/**
+ * @author kuyur@kuyur.info
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
  * min-heap class.
  * @author kuyur@kuyur.info
  * @param {number} capacity

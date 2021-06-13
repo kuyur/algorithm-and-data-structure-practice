07b89901879dffa0c2b5cad5549870839dec11e0
kuyur <kuyur@kuyur.info>
2021-06-13 17:54:05 +0900

    add description of leetcode 004.

--- leetcode/leetcode-004/solution.js
+++ leetcode/leetcode-004/solution.js
@@ -1,4 +1,19 @@
 /**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ *
+ * The overall run time complexity should be O(log (m+n)).
+ * 
+ * Example:
+ * Input: nums1 = [1,2], nums2 = [3,4]
+ * Output: 2.50000
+ * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ */
+
+/**
+ * @author kuyur@kuyur.info
+ */
+
+/**
  * Assume that arr1 and arr2 are non-empty and sorted in ascending order.
  * @param {Array.<number>} arr1
  * @param {Array.<number>} arr2

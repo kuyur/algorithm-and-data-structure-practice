6cad407944cd5b27afc638ba7af108b174f1e531
kuyur <kuyur@kuyur.info>
2021-06-13 17:55:28 +0900

    add description of leetcode 021.

--- leetcode/leetcode-021/solution.js
+++ leetcode/leetcode-021/solution.js
@@ -1,4 +1,21 @@
 /**
+ * Merge two sorted linked lists and return it as a sorted list.
+ * The list should be made by splicing together the nodes of the first two lists.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @author kuyur@kuyur.info
+ */
+
+/**
  * @param {{val: number, next: {}}} l1
  * @param {{val: number, next: {}}} l2
  * @return {{val: number, next: {}}}
